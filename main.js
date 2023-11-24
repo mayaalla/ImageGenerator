697d08118a2efea608dffa9a8ced1565cdebee56
Maya Alla <102388977+mayaalla@users.noreply.github.com>
2023-11-24 19:10:50 +0100

    Update main.js

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ let three = document.getElementById("3");
 let four = document.getElementById("4");
 let gnrt = document.getElementById("gnrt");
 let err = document.getElementById("err")
-const key = "Bearer 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";
+const key = "Bearer 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";
 let k;
 let el;
 let array = [];
